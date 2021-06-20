701e7ceb5522fb431c91becd0a6f438e92cd98ac
Kamil Szewc <kamil.szewc@gmail.com>
2021-06-20 14:25:47 +0200

    Added cors configuration

--- src/main/java/eu/integrable/storagemanager/configuration/WebSecurityConfig.java
+++ src/main/java/eu/integrable/storagemanager/configuration/WebSecurityConfig.java
@@ -3,11 +3,17 @@ package eu.integrable.storagemanager.configuration;
 import eu.integrable.storagemanager.security.JwtAuthorizationFilter;
 import eu.integrable.storagemanager.security.JwtTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +32,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api-docs.*").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .cors()
+                .and()
                 .addFilterBefore(new JwtAuthorizationFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/api/v1/**", corsConfiguration);
+        return urlBasedCorsConfigurationSource;
     }
 }
